627a02ca81e53994b586e22651e757544540a94b
lymperis-e <76447999+lymperis-e@users.noreply.github.com>
2022-03-30 05:02:00 +0300

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1xX5Ho9QLJnnHQP3Zlyy242ejuk_NyQrpzu-zmk0PZMU/edit#gid=226407370';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
+var googleApiKey = 'AIzaSyDxVFl5JV8Th6j6L5EkZyQ-ucvutrFOpY4';
