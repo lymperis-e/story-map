f53206e7b1ce966be0dc45434bd3d428737f5d47
lymperis-e <76447999+lymperis-e@users.noreply.github.com>
2022-03-30 16:16:53 +0300

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,2 +1,2 @@
- var googleDocURL = 'https://docs.google.com/spreadsheets/d/1xX5Ho9QLJnnHQP3Zlyy242ejuk_NyQrpzu-zmk0PZMU/edit#gid=226407370';
- var googleApiKey = 'AIzaSyDxVFl5JV8Th6j6L5EkZyQ-ucvutrFOpY4';
+ var googleDocURL = '${{ secrets.G_SHEET }}';
+ var googleApiKey = '${{ secrets.GCC_API  }}';
