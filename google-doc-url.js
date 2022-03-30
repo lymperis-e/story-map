c448f3ba27c0ff5be71dfc2c5f43e4e875cc43ef
lymperis-e <76447999+lymperis-e@users.noreply.github.com>
2022-03-30 05:22:59 +0300

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,2 @@
-// paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = '${{ secrets.G_SHEET }}';
-
-// insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = '${{ secrets.GCC_API }}';
+ var googleDocURL = 'https://docs.google.com/spreadsheets/d/1xX5Ho9QLJnnHQP3Zlyy242ejuk_NyQrpzu-zmk0PZMU/edit#gid=226407370';
+ var googleApiKey = 'AIzaSyDxVFl5JV8Th6j6L5EkZyQ-ucvutrFOpY4';
